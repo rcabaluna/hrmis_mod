e28770d69100b6edaa6a9be5fcfe37ab0d11d95b
Ruel Cabaluna Jr <rcabalunajr@gmail.com>
2025-02-19 15:35:43 +0800

    02192025

--- application/modules/officer/controllers/Tasks.php
+++ application/modules/officer/controllers/Tasks.php
@@ -30,17 +30,11 @@ class Tasks extends MY_Controller
 
 		$requestFlow = $this->Request_model->getRequestFlow($officer_empid);
 
-		
-
-
-	
 
 		$allemp_request = array();
 		$arremp_request = $this->Request_model->getEmployeeRequest(curryr(), currmo());	
-
 		
 		$arrRequest = $this->Notification_model->check_request_flow_and_signatories2($requestFlow, $arremp_request,$office);
-
 
 		foreach ($arrRequest as $req) :
 
