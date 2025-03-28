971ce885e754f850f5e0721677253965397ddd94
Ruel Cabaluna Jr <rcabalunajr@gmail.com>
2025-03-28 08:34:15 +0800

    Updated Travel Order View

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -612,7 +612,7 @@ if (!function_exists('generate_travel_order_no')) {
 
         $count = $CI->db->where('requestCode', 'TO')->count_all_results('tblemprequest');
         
-        return date('Y') . '-' . str_pad($count+1, 4, '0', STR_PAD_LEFT);
+        return 'H'.date('Y') . '-' . str_pad($count+1, 4, '0', STR_PAD_LEFT);
         
     }
 }
